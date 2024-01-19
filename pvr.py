111f7104d83a3dc0c64946afb7fa2c0d3356685f
tkwa <kwathomas0@gmail.com>
2024-01-19 03:12:33 +0000

    continue implementing MNIST-PVR IIT

--- pvr.py
+++ pvr.py
@@ -5,15 +5,20 @@ The MNIST-PVR task. We need to download the MNIST dataset and process it.
 
 import numpy as np
 import torchvision.datasets as datasets
+import torch as t
+import torchvision
 from torch.utils.data import Dataset
 from PIL import Image
 from typing import Optional
+from transformer_lens.hook_points import HookedRootModule, HookPoint
 
 # %%
 
 mnist_train = datasets.MNIST("./data", download=True)
 mnist_test = datasets.MNIST("./data", train=False, download=True)
 # %%
+
+MNIST_CLASS_MAP = {k: [1, 1, 1, 1, 2, 2, 2, 3, 3, 3][k] for k in range(10)}
 
 class ImagePVRDataset(Dataset):
     """
@@ -24,7 +29,7 @@ class ImagePVRDataset(Dataset):
     def __init__(self, base_dataset, class_map:Optional[dict[int, int]]=None, seed=0, use_cache=False):
         self.base_dataset = base_dataset
         if class_map is None:
-            class_map = {k: [1, 1, 1, 1, 2, 2, 2, 3, 3, 3][k] for k in range(10)}
+            class_map = MNIST_CLASS_MAP
         self.class_map = class_map
         self.rng = np.random.default_rng(seed)
         assert all(v in {1, 2, 3} for v in class_map.values())
@@ -52,11 +57,13 @@ class ImagePVRDataset(Dataset):
             return self.cache[index]
         images = [self.base_dataset[i][0] for i in range(index, index + 4)]
         new_image = self.concatenate_2x2(images)
+        new_image = torchvision.transforms.functional.to_tensor(new_image)
 
         base_label = self.base_dataset[index][1]
         pointer = self.class_map[base_label]
         new_label = self.base_dataset[index + pointer][1]
-        return new_image, new_label
+        intermediate_vars = (self.base_dataset[index + i][1] for i in range(0, 3))
+        return new_image, new_label, intermediate_vars
     
     def __len__(self):
         return len(self.base_dataset) // 4
@@ -66,4 +73,31 @@ class ImagePVRDataset(Dataset):
 
 mnist_pvr_train = ImagePVRDataset(mnist_train, None)
 mnist_pvr_test = ImagePVRDataset(mnist_test, None)
+# %%
+
+class MNIST_PVR_HL(HookedRootModule):
+    """
+    A high-level implementation of the algorithm used for MNIST_PVR
+    """
+    def __init__(self, class_map=MNIST_CLASS_MAP):
+        super().__init__()
+        self.hook_tl = HookPoint()
+        self.hook_tr = HookPoint()
+        self.hook_bl = HookPoint()
+        self.hook_br = HookPoint()
+        self.class_map = class_map
+        self.setup()
+
+    def forward(self, intermediate_data):
+        tl, tr, bl, br = intermediate_data
+        tl = self.hook_tl(tl)
+        tr = self.hook_tr(tr)
+        bl = self.hook_bl(bl)
+        br = self.hook_br(br)
+        pointer = self.class_map[tl]
+        return [tr, bl, br][pointer]
+
+# %%
+hl = MNIST_PVR_HL()
+hl.hook_dict
 # %%
--- train.py
+++ train.py
@@ -4,12 +4,15 @@ import numpy as np
 import torch as t
 from torch import Tensor
 from torch.utils.data import Dataset
+import torchvision
 import transformer_lens as tl
 from transformer_lens.hook_points import HookedRootModule, HookPoint
 import networkx as nx
 from wrapper import HookedModuleWrapper
 from typing import Callable, Optional
-from pvr import mnist_pvr_train, mnist_pvr_test
+import wandb
+
+from pvr import mnist_pvr_train, mnist_pvr_test, MNIST_PVR_HL
 
 
 # %%
@@ -41,7 +44,11 @@ class LLNode():
     subspace: Optional[t.Tensor]
 
 class IITDataset(Dataset):
+    """
+    Each thing is randomly sampled from a pair of datasets.
+    """
     def __init__(self, base_data, ablation_data, seed=0):
+        # For vanilla IIT, base_data and ablation_data are the same
         self.base_data = base_data
         self.ablation_data = ablation_data
         self.seed = seed
@@ -49,12 +56,16 @@ class IITDataset(Dataset):
     def __getitem__(self, index):
         # sample based on seed
         rng = np.random.default_rng(self.seed * 1000000 + index)
-        base_input = rng.choice(self.base_data)
-        ablation_input = rng.choice(self.ablation_data)
+        base_index = rng.choice(len(self.base_data))
+        ablation_index = rng.choice(len(self.ablation_data))
+
+        base_input = self.base_data[base_index]
+        ablation_input = self.ablation_data[ablation_index]
         return base_input, ablation_input
 
     def __len__(self):
         return len(self.base_data)
+
 
 class IITModelPair():
     hl_model: HookedRootModule
@@ -64,7 +75,8 @@ class IITModelPair():
     hl_graph: nx.DiGraph
     corr: dict[HookName, set[HookName]] # high -> low correspondence. Capital Pi in paper
 
-    def __init__(self, hl_model=None, ll_model=None, hl_graph=None, corr:dict[HLNode, set[LLNode]]={}, seed=0):
+    def __init__(self, hl_model:HookedRootModule=None, ll_model:HookedRootModule=None,
+                 hl_graph=None, corr:dict[HLNode, set[LLNode]]={}, seed=0, training_args={}):
         # TODO change to construct hl_model from graph?
         if hl_model is None:
             assert hl_graph is not None
@@ -72,9 +84,11 @@ class IITModelPair():
 
         self.hl_model = hl_model
         self.ll_model = ll_model
+
         self.corr = corr
-        assert all([k in self.hl_model.hook_points for k in self.corr.keys()])
+        assert all([k in self.hl_model.hook_dict for k in self.corr.keys()])
         self.rng = np.random.default_rng(seed)
+        self.training_args = training_args
 
     def make_hl_model(self, hl_graph):
         raise NotImplementedError
@@ -94,29 +108,67 @@ class IITModelPair():
         return out
 
     def do_intervention(self, base_input, ablation_input, hl_node:HookName):
+        base_x, base_y, base_intermediate_vars = base_input
+        ablation_x, ablation_y, ablation_intermediate_vars = ablation_input
         hl_ablation_output, self.hl_cache = self.hl_model.run_with_cache(ablation_input)
         ll_ablation_output, self.ll_cache = self.ll_model.run_with_cache(ablation_input)
 
         hl_name = self.sample_hl_name()
-        ll_name = self.corr(hl_name) # TODO change to multiple names
+        ll_names = self.corr(hl_name)
 
-        hl_output = self.hl_model.run_with_hooks(base_input, (hl_name, self.hl_ablation_hook))
-        ll_output = self.ll_model.run_with_hooks(base_input, (ll_name, self.ll_ablation_hook))
+        hl_output = self.hl_model.run_with_hooks(base_input, [(hl_name, self.hl_ablation_hook)])
+        ll_output = self.ll_model.run_with_hooks(base_input, [(ll_name, self.ll_ablation_hook) for ll_name in ll_names])
 
         return hl_output, ll_output
 
-    def train(self, base_data, ablation_data, hl_node:HookName, n_examples=1000):
+    def train(self, base_data, ablation_data, epochs=1000, use_wandb=False):
         dataset = IITDataset(base_data, ablation_data)
-        loss_fn = t.nn.MSELoss()
-        for example in range(n_examples):
-            # sample one base and one ablation datapoint
-            base_input, ablation_input = dataset[example]
+        print(self.ll_model)
+        print(list(self.ll_model.parameters()))
+        optimizer = t.optim.Adam(self.ll_model.parameters(), lr=self.training_args['lr'])
+        loss_fn = t.nn.CrossEntropyLoss()
 
-            # sample a high-level variable to ablate
-            hl_output, ll_output = self.do_intervention(base_data, ablation_data, hl_node)
+        if use_wandb:
+            raise NotImplementedError
+
+        for epoch in range(epochs):
+            losses = []
+            for i in range(len(dataset)):
+                optimizer.zero_grad()
+                self.hl_model.requires_grad_(False)
+                self.ll_model.train()
+                # sample one base and one ablation datapoint
+                base_input, ablation_input = dataset[i]
+
+                # sample a high-level variable to ablate
+                hl_node = self.sample_hl_name()
+                hl_output, ll_output = self.do_intervention(base_input, ablation_input, hl_node)
+                loss = loss_fn(ll_output, hl_output)
+                loss.backward()
+                print(f"{ll_output=}, {hl_output=}")
+                losses.append(loss.item())
+                optimizer.step()
+            print(f"Epoch {epoch}: {np.mean(losses)}")
+            if use_wandb:
+                wandb.log({'loss': np.mean(losses), 'epoch': epoch})
 
 
 # %%
+            
+resnet18 = torchvision.models.resnet18(pretrained=False) # 11M parameters
+wrapped_r18 = HookedModuleWrapper(resnet18, name='resnet18', recursive=True, hook_self=False)
 
-if __name__ == '__main__':
-    pass
+# %%
+
+training_args = {
+    'lr': 0.001
+}
+
+
+model_pair = IITModelPair(hl_model=MNIST_PVR_HL(), ll_model=wrapped_r18, seed=0, training_args=training_args)
+model_pair.train(mnist_pvr_train, mnist_pvr_train, epochs=10)
+
+# %%
+
+wrapped_r18(t.randn(1, 3, 56, 56)).shape
+# %%
