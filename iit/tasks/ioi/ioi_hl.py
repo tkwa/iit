ff7f8bf5159b075cb1f773a6cc04244df79bd2c0
Thomas Kwa <kwathomas0@gmail.com>
2024-02-28 18:30:54 -0800

    fixes to ioi

--- iit/tasks/ioi/ioi_hl.py
+++ iit/tasks/ioi/ioi_hl.py
@@ -2,6 +2,7 @@
 import transformer_lens
 
 from iit.model_pairs.base_model_pair import HookName
+import numpy as np
 import torch as t
 from transformer_lens.hook_points import HookedRootModule, HookPoint
 from iit.utils.config import DEVICE
@@ -18,18 +19,18 @@ IOI_NAMES = t.tensor([10, 20, 30]) # TODO
 class DuplicateHead(t.nn.Module):
     def forward(self, tokens:t.Tensor):
         # Write the last previous position of any duplicated token (used at S2)
-        positions = (tokens == tokens[:, None])
-        positions = t.triu(positions, diagonal=1)
+        positions = (tokens[..., None, :] == tokens[..., :, None]) # batch seq1 seq2
+        positions = t.triu(positions, diagonal=1) # only consider positions before this one
         indices = positions.nonzero(as_tuple=True)
         ret = t.full_like(tokens, -1)
-        ret[indices[1]] = indices[0]
+        ret[indices[0], indices[2]] = indices[1]
         return ret
     
 class PreviousHead(t.nn.Module):
     def forward(self, tokens:t.Tensor):
         # copy token S1 to token S1+1 (used at S1+1)
         ret = t.full_like(tokens, -1)
-        ret[1:] = tokens[:-1]
+        ret[..., 1:] = tokens[..., :-1]
         return ret
 
 class InductionHead(t.nn.Module):
@@ -57,14 +58,20 @@ class NameMoverHead(t.nn.Module):
         """
         increase logit of all names in the sentence, except those flagged by s_inhibition
         """
-        logits = t.zeros((len(tokens), self.d_vocab_out)) # seq d_vocab
+        batch, seq = tokens.shape
+        logits = t.zeros((batch, seq, self.d_vocab_out)) # batch seq d_vocab
         # we want every name to increase its corresponding logit after it appears
-        name_mask = tokens.eq(self.names[:, None]).any(dim=0)
-        logits[t.arange(len(tokens)), tokens] = 10 * name_mask.float()
+        name_mask = tokens.eq(self.names[None, :, None]).any(dim=1)
+        print(name_mask)
+        batch_indices, seq_indices = t.meshgrid(t.arange(batch), t.arange(seq))
+        logits[batch_indices, seq_indices, tokens] = 10 * name_mask.float()
         # now decrease the logit of the names that are inhibited
-        logits[t.arange(len(tokens)), s_inhibition] += -15 * s_inhibition.ne(-1).float()
-        logits = t.cumsum(logits, dim=0)
+        logits[batch_indices, seq_indices, s_inhibition] += -15 * s_inhibition.ne(-1).float()
+        logits = t.cumsum(logits, dim=1)
         return logits
+    
+# since 0, 3 contains 20, we write
+# a 1 to position 0, 3, 20 of logits
         
 # %%
         
@@ -109,6 +116,10 @@ class IOI_HL(HookedRootModule):
     def forward(self, args, verbose=False):
         show = print if verbose else lambda *args, **kwargs: None
         input, label, _intermediate_data = args
+        batched = True
+        if len(input.shape) == 1:
+            batched = False
+            input = input[None, ...]
         # print([a.shape for a in args])
         # duplicate, previous, induction, s_inhibition, name_mover = [intermediate_data[:, i] for i in range(5)]
         # print(f"intermediate_data is a {type(intermediate_data)}; duplicate is a {type(duplicate)}")
@@ -123,5 +134,7 @@ class IOI_HL(HookedRootModule):
         show(f"s_inhibition: {s_inhibition}")
         out = self.name_mover_head(input, s_inhibition)
         out = self.hook_name_mover(out)
+        if not batched:
+            out = out[0]
         return out
 # %%
--- iit/tasks/ioi/test_ioi.py
+++ iit/tasks/ioi/test_ioi.py
@@ -6,24 +6,24 @@ def nonzero_values(a: t.Tensor):
     return t.cat((a.nonzero(), a[a != 0][:, None]), dim=-1)
 
 def test_duplicate_head():
-    a = DuplicateHead()(t.tensor([3, 1, 4, 1, 5, 9, 2, 6, 5]))
-    assert a.equal(t.tensor([-1, -1, -1,  1, -1, -1, -1, -1,  4]))
+    a = DuplicateHead()(t.tensor([[3, 1, 4, 1, 5, 9, 2, 6, 5]]))
+    assert a.equal(t.tensor([[-1, -1, -1,  1, -1, -1, -1, -1,  4]]))
 
 
 def test_previous_head():
-    a = PreviousHead()(t.tensor([3, 1, 4, 1, 5, 9, 2, 6, 5]))
-    assert a.equal(t.tensor([-1,  3,  1,  4,  1,  5,  9,  2,  6]))
+    a = PreviousHead()(t.tensor([[3, 1, 4, 1, 5, 9, 2, 6, 5]]))
+    assert a.equal(t.tensor([[-1,  3,  1,  4,  1,  5,  9,  2,  6]]))
 
   
 def test_s_inhibition_head():
-    a = SInhibitionHead()(t.tensor([3, 1, 4, 1, 5, 9, 2, 6, 5]), t.tensor([-1, -1, -1,  1, -1, -1, -1, -1,  4]))
-    assert a.equal(t.tensor([-1, -1, -1,  1, -1, -1, -1, -1,  5]))
+    a = SInhibitionHead()(t.tensor([[3, 1, 4, 1, 5, 9, 2, 6, 5]]), t.tensor([[-1, -1, -1,  1, -1, -1, -1, -1,  4]]))
+    assert a.equal(t.tensor([[-1, -1, -1,  1, -1, -1, -1, -1,  5]]))
 
 
 def test_name_mover_head():
-    a = NameMoverHead(d_vocab=21)(t.tensor([1, 2, 10, 20]), t.tensor([-1, 20, 10, -1]))
+    a = NameMoverHead(d_vocab=21)(t.tensor([[1, 2, 10, 20]]), t.tensor([[-1, 20, 10, -1]]))
 
-    assert nonzero_values(a).equal(t.tensor(
+    assert nonzero_values(a[0]).equal(t.tensor(
         [[  1.,  20., -15.],
         [  2.,  10.,  -5.],
         [  2.,  20., -15.],
@@ -32,8 +32,8 @@ def test_name_mover_head():
 
 
 def test_ioi_hl():
-    a = IOI_HL(device='cuda')((t.tensor([3, 10, 4, 10, 5, 9, 2, 6, 5]), None, None))
-    assert nonzero_values(a).equal(t.tensor([[  1.,  10.,  10.],
+    a = IOI_HL(device='cuda')((t.tensor([[3, 10, 4, 10, 5, 9, 2, 6, 5]]), None, None))
+    assert nonzero_values(a[0]).equal(t.tensor([[  1.,  10.,  10.],
         [  2.,  10.,  10.],
         [  3.,  10.,   5.],
         [  4.,  10.,   5.],
